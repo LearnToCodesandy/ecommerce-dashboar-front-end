db29c9679b67906158c4c2e94e68dc200629b665
LearnToCodesandy <nelapudisandeepofficial@gmail.com>
2022-04-13 23:16:37 +0530

    3 files modified

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
 
 const Login = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <>
       <div className="login__wrapper">
-        <form className="form__container login">
+        <h1 className="title">
+          {!toggle ? 'Hi, Nice to see you back!' : 'Welcome to our services!'}
+        </h1>
+        <form className={!toggle ? 'form__container login' : 'display-hide'}>
           <div className="form__item">
             <label htmlFor="username">Username</label>
             <input
@@ -22,9 +27,14 @@ const Login = () => {
           </div>
 
           <input type="submit" value="Login" />
+
+          <p className="under__text">
+            Need an account?{' '}
+            <span onClick={() => setToggle(!toggle)}>Sign up</span>
+          </p>
         </form>
 
-        <form className="form__container signup">
+        <form className={toggle ? 'form__container signup' : 'display-hide'}>
           <div className="form__item">
             <label htmlFor="email">Email Id</label>
             <input
@@ -64,6 +74,11 @@ const Login = () => {
           </div>
 
           <input type="submit" value="Sign Up" />
+
+          <p className="under__text">
+            Already have an account?{' '}
+            <span onClick={() => setToggle(!toggle)}>Sign in</span>
+          </p>
         </form>
       </div>
     </>
